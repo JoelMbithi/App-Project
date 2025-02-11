1c2cdf814a6224b32033421c3b3e79490fcc8ff8
Joel Mbithi <joellembithi@gmail.com>
2025-02-11 22:33:06 +0300

    done

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -3,18 +3,20 @@ import {assets} from '../assets/assets'
 
 const About = () => {
   return (
-    <div>
-      <div>
-        <p>ABOUT <span>US</span> </p>
+    <div >
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span> </p>
       </div>
 
-      <div>
-       <img src={assets.about_image} alt='' />
-      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+       <img className='w-full md:max-w-[350px]' src={assets.about_image} alt='' />
+      <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
       <p>At DigiCare Doc, we are dedicated to revolutionizing healthcare by providing seamless access to top medical professionals from the comfort of your home. Our online platform connects patients with experienced doctors for virtual consultations, ensuring timely medical advice, diagnosis, and treatment—anytime, anywhere.</p>
       <p>We believe in accessible, affordable, and quality healthcare for all. Whether you need a routine check-up, specialist consultation, or urgent medical guidance, our trusted healthcare professionals are just a click away.</p>
-      <br>Our Vision</br>
+      <b>Our Vision</b>
       <p>To be the leading digital healthcare platform, transforming the way patients access medical care by providing seamless, efficient, and patient-centered online consultations. We envision a world where quality healthcare is accessible to everyone, anytime, anywhere, ensuring better health outcomes and enhanced well-being for all.</p>
+    </div>
+    </div>
     </div>
   )
 }
