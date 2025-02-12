2d04958f9ba64b54b3624ba2b411638acc298d6f
Joel Mbithi <joellembithi@gmail.com>
2025-02-12 21:36:14 +0300

    still doing contact page

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Contact = () => {
   return (
     <div>
       
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'>US</span> </p>
+      </div>
+
+      <div className='my-10 flex flex-col justfy-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt='' />
+
+        <div className='flex flex-col justfy-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>Our OFFICE</p>
+          <p className='text-gray-500'>Embakasi<br /> Tel Aviv, Nairobi</p>
+          <p  className='text-gray-500'>Tel: (+254) 743-861-565 <br/> Email: joellembithi@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>Careers at DigiCare Doc</p>
+          <p className='text-gray-500'>Learn more about our teams and jobs openings.</p>
+          <button>Explore Jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
