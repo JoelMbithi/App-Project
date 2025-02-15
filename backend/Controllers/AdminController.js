9941d751f2d3cd66ad338b810bad99c9f487fe30
Joel Mbithi <joellembithi@gmail.com>
2025-02-15 09:52:55 +0300

    still doing middleware

---
+++ backend/Controllers/AdminController.js
@@ -0,0 +1,16 @@
+
+
+
+//ApI to adding doctors
+
+const addDoctor = async(req,res) => {
+    try {
+
+        const { name, email, password, speciality, about, fees, address } = req.body
+        
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
