64ffb87ab5431ea7386b848ac47d4586594fa9ae
Joel Mbithi <joellembithi@gmail.com>
2025-02-16 12:24:12 +0300

    done one API and is working well

--- backend/Controllers/AdminController.js
+++ backend/Controllers/AdminController.js
@@ -1,16 +1,15 @@
-
-
-
-//ApI to adding doctors
-
-const addDoctor = async(req,res) => {
+// API to add doctors
+const addDoctor = async (req, res) => {
     try {
+        const { name, email, password, speciality, about, fees, address } = req.body;
+        const imageFile = req.file;
 
-        const { name, email, password, speciality, about, fees, address } = req.body
-        
+        console.log({ name, email, password, speciality, about, fees, address }, imageFile);
+        res.status(200).json({ message: "Doctor added successfully" });
     } catch (error) {
-        
+        console.error("Error in addDoctor:", error);
+        res.status(500).json({ message: "Server Error" });
     }
-}
+};
 
-export {addDoctor}
+export { addDoctor };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './Config/MongoDB.js';
 import connectCloudinary from './Config/Cloudinary.js';
+import adminRouter from './Routes/AdminRoute.js';
 
 // App Config
 const app = express();
@@ -19,6 +20,9 @@ connectDB();
 connectCloudinary()
 
 // API Endpoints
+
+app.use('/api/admin', adminRouter)
+
 app.get('/', (req, res) => {
   res.send('API WORKING WELL JOE');
 });
