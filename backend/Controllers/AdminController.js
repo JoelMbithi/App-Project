fa4cc1b87a7367066604cb2425cb81c9c52f3fa1
Joel Mbithi <joellembithi@gmail.com>
2025-02-17 11:40:18 +0300

    doing validfation to addDoctor Details

--- backend/Controllers/AdminController.js
+++ backend/Controllers/AdminController.js
@@ -1,14 +1,29 @@
+import validator from 'validator'
+
+
 // API to add doctors
 const addDoctor = async (req, res) => {
     try {
-        const { name, email, password, speciality, about, fees, address } = req.body;
+        const { name, email, password, speciality, experience, about, fees, address } = req.body;
         const imageFile = req.file;
 
-        console.log({ name, email, password, speciality, about, fees, address }, imageFile);
-        res.status(200).json({ message: "Doctor added successfully" });
+        // checking for all data to add a doctor
+        if (!name || !email || !password || !speciality || !experience || !about || !fees || !address) {
+            return res.json({ success: false, message: "Missing Details" })
+        }
+
+        //validation of email format
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Pleae Enter a valid email" })
+        }
+
+        //validating strong password  Check if password is at least 8 characters long at least one uppercase letter at least one digit v at least one special character
+        if (password.length < 8 || !/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+            return res.json({ success: false, message: "Password is weak. Please enter a strong password." });
+        }
+
     } catch (error) {
-        console.error("Error in addDoctor:", error);
-        res.status(500).json({ message: "Server Error" });
+     
     }
 };
 
